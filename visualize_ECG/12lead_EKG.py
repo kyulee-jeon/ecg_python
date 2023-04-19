ef0160d39540ee6b70cb698bd9c7c461994c6716
Kyulee Jeon <97151697+kyulee-jeon@users.noreply.github.com>
2023-04-19 15:53:44 +0900

    Update 12lead_EKG.py

--- visualize_ECG/12lead_EKG.py
+++ visualize_ECG/12lead_EKG.py
@@ -81,3 +81,37 @@ def show_12_wvform(df, X_data, y_data, i):
 
     
 show_12_wvform(df_test, X_test, y_test, 3)
+
+#################################################################################################################
+# Save 12 lead waveform
+def save_12_wvform(directory, Title, df, X_data, y_data, i):
+    stecg = cal_12lead(X_data, y_data)
+    chartno_ls, dt_ls, ecgid_ls = make_ls(df)
+    size = (40,45)
+    font_size = 25
+    fig, ax = plt.subplots(12, 1)   
+    plt.suptitle('['+ str(i) +']   '+'[ChartNo] '+ str(chartno_ls[i]) + '   [Date] ' + str(dt_ls[i])+ '   [ECG ID] ' + str(ecgid_ls[i]) + '     ('+str(Title)+')', position=(0.5,0.9), fontweight='bold',fontsize=font_size*1.5)
+    
+    df_x = pd.DataFrame(stecg[i], columns=['I', 'II', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'III', 'aVR', 'aVL','aVF']) # i: serial number of BBB list (0-359)
+    df_x = df_x[['I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']]
+    lead_nm = df_x.columns
+    for j, nm in enumerate(lead_nm):
+        lead = df_x[nm].to_numpy()
+        ax[j].plot(lead)
+        ax[j].set_title(nm, fontweight="bold", size=font_size)
+        plt.rcParams["figure.figsize"] = size
+    
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.85)
+    plt.show()
+    
+    # save it
+    filepath = directory + str(Title)
+    filename = "{}_{:03d}.png".format(str(Title), i)
+    fig.savefig(filepath + filename, dpi=fig.dpi)
+    
+    
+   
+
+
+
